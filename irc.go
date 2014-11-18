ff472e90bbabbb250a948b0433d632f20c02d2e9
fro_ozen <fro_ozen@gmx.de>
2014-11-18 22:23:35 +0100

    Implement irc.Parse(signal string)

--- irc.go
+++ irc.go
@@ -1,6 +1,84 @@
 // irc provides an implementation of the Internet Relay Chat protocol.
 // On top of that, it offers some shortcuts for easier and more comfortable
 // usage.
+//
 // The implementation tries to comply to this specification:
 // http://tools.ietf.org/html/rfc1459
 package irc
+
+import (
+	"errors"
+	"fmt"
+	"regexp"
+	"strings"
+)
+
+var (
+	nickMask *regexp.Regexp = regexp.MustCompile(":(.*?)!?(.*?)@?(.*)")
+)
+
+// IRCEvent represents a parsed IRC signal
+type IRCEvent struct {
+	// Data representing the signal origin
+	Name string
+	User string
+	Host string
+
+	// Data representing the contents of the signal
+	Type      string
+	Arguments []string
+}
+
+// Parse parses an IRC signal and returns an *IRCEvent and an error
+func Parse(signal string) (*IRCEvent, error) {
+	signalArgs := strings.Split(signal, " ")
+
+	// The next parsing steps definetly won't fail if this passes
+	if len(signalArgs) < 2 {
+		return nil, errors.New(fmt.Sprintf("\"%s\": Not enough arguments."))
+	}
+
+	ircEvent := new(IRCEvent)
+
+	// Signal has source
+	if strings.HasPrefix(signalArgs[0], ":") {
+		// Structure :nick!user@host (only nick is guaranteed, though)
+		source := strings.TrimPrefix(signalArgs[0], ":")
+
+		// Source contains user
+		if strings.Contains(source, "!") {
+			split := strings.Split(source, "!")
+			// Source contains host as well
+			if strings.Contains(split[1], "@") {
+				spilt2 := strings.Split(split[1], "@")
+				ircEvent.User = spilt2[0]
+				ircEvent.Host = spilt2[1]
+				source = split[0]
+			} else {
+				ircEvent.User = split[1]
+				source = split[0]
+			}
+		}
+
+		// Source contains host
+		if strings.Contains(source, "@") {
+			split := strings.Split(source, "@")
+			ircEvent.Host = split[1]
+			source = split[0]
+		}
+
+		// At this point, only the name will remain in source
+		ircEvent.Name = source
+
+		// This means the second argument is the type
+		ircEvent.Type = signalArgs[1]
+
+		ircEvent.Arguments = signalArgs[2:]
+	} else {
+		ircEvent.Type = signalArgs[0]
+
+		ircEvent.Arguments = signalArgs[1:]
+	}
+
+	return ircEvent, nil
+}
---
+++ irc_test.go
@@ -0,0 +1,55 @@
+package irc
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestParse(t *testing.T) {
+	if _, err := Parse("JOIN"); err == nil {
+		t.Error("Accepts signals with to few arguments.")
+	}
+
+	parseTest(t, ":nick!user@host JOIN", "nick", "user", "host", "JOIN")
+	parseTest(t, ":nick@host JOIN", "nick", "", "host", "JOIN")
+	parseTest(t, ":nick!user JOIN", "nick", "user", "", "JOIN")
+	parseTest(t, "JOIN #channel", "", "", "", "JOIN", "#channel")
+	parseTest(t, ":node.server.net PING node.server.net", "node.server.net", "",
+		"", "PING", "node.server.net")
+}
+
+func parseTest(t *testing.T, signal, name, user, host, _type string, args ...string) {
+	fmt.Println("Testing:", signal)
+
+	event, err := Parse(signal)
+	if err != nil {
+		t.Error("Parsing failed:", err)
+	}
+
+	if event.Name != name {
+		t.Errorf("Parsing name failed: parsed: %s, expected %s", event.Name, name)
+	}
+
+	if event.User != user {
+		t.Errorf("Parsing user failed: parsed: %s, expected %s", event.User, user)
+	}
+
+	if event.Host != host {
+		t.Errorf("Parsing host failed: parsed: %s, expected %s", event.Host, host)
+	}
+
+	if event.Type != _type {
+		t.Errorf("Parsing type failed: parsed: %s, expected %s", event.Type, _type)
+	}
+
+	if len(args) != len(event.Arguments) {
+		t.Errorf("Parsing arguments failed: parsed: %s, expected %s", event.Arguments, args)
+		return
+	}
+
+	for i, arg := range args {
+		if event.Arguments[i] != arg {
+			t.Errorf("Parsing arguments failed: parsed: %s, expected %s", event.Arguments, args)
+		}
+	}
+}
