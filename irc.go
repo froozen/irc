53f570b2e6b58062684ca78e12fcf58d513dab1d
fro_ozen <fro_ozen@gmx.de>
2014-11-18 18:50:14 +0100

    Initial Commit

---
+++ irc.go
@@ -0,0 +1,6 @@
+// irc provides an implementation of the Internet Relay Chat protocol.
+// On top of that, it offers some shortcuts for easier and more comfortable
+// usage.
+// The implementation tries to comply to this specification:
+// http://tools.ietf.org/html/rfc1459
+package irc
